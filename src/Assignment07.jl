60fe8f9733ba9efe190b51763d2cb6bd8e69381b
Anika Luo <al8@wellesley.edu>
2021-06-29 16:59:57 -0400

    composition

--- src/Assignment07.jl
+++ src/Assignment07.jl
@@ -1,6 +1,7 @@
 module Assignment07
 
-export normalizeDNA
+export normalizeDNA,
+        composition
 
 # # uncomment the following line if you intend to use BioSequences types
 # using BioSequences
@@ -16,7 +17,7 @@ function normalizeDNA(seq)
     seq = uppercase(string(seq))
     for base in seq
         # note: `N` indicates an unknown base
-        occursin(base, "AGCTN") || error("invalid base $base")
+        occursin(base, "AGCTN") || error("Invalid base $base")
     end
     return seq # change to `return LongDNASeq(seq)` if you want to try to use BioSequences types
 end
@@ -25,5 +26,42 @@ end
 # Your code here.
 # Don't forget to export your functions!
 
+
+"""
+    composition(sequence)
+Counts the number of each type of base
+in a DNA sequence and returns a Dict
+Examples  
+≡≡≡≡≡≡≡≡≡≡
+    julia> composition("ACCGGGTTTTN")
+    Dict{Char,Int64} with 5 entries:
+        'A' => 1
+        'G' => 3
+        'T' => 4
+        'N' => 1
+        'C' => 2
+        
+    julia> composition("AAX")
+    ERROR: Invalid base, X
+"""
+function composition(sequence)
+    sequence = normalizeDNA(sequence)
+    comp = Dict('A' => 0, 'C' => 0, 'G' => 0, 'T' => 0, 'N' => 0)
+    for base in sequence
+        # add 1 to each base as it occurs
+        if base == 'A'
+            comp['A'] = comp['A'] + 1
+        elseif base == 'C'
+            comp['C'] = comp['C'] + 1
+        elseif base == 'G'
+            comp['G'] = comp['G'] + 1
+        elseif base == 'T'
+            comp['T'] = comp['T'] + 1
+        elseif base == 'N'
+            comp['N'] = comp['N'] + 1
+        end
+    end
+    return comp
+end
 
 end # module Assignment07
